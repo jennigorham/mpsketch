c7fd804a1decbcbfe2a547ccfa86002878f2ba3e
Jenni Gorham <jenni.gorham@gmail.com>
2017-01-09 10:38:45 +1100

    Gtk scrolling and getopt

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ mpsketch: mpsketch.c paths.c paths.h mptoraster.c mptoraster.h
 	gcc -Wall mpsketch.c paths.c mptoraster.c -L/usr/X11R6/lib -lX11 -L mplib -l mplib -l kpathsea -l mputil -lcairo -lpixman -lpng -lz -lmpfr -lgmp -lm -o mpsketch
 
 gtk-test: gtk-test.c
-	gcc -Wall `pkg-config --cflags gtk+-3.0` gtk-test.c `pkg-config --libs gtk+-3.0` -o gtk-test
+	gcc -Wall `pkg-config --cflags gtk+-3.0` gtk-test.c paths.c `pkg-config --libs gtk+-3.0` -L mplib -l mplib -l kpathsea -l mputil -lcairo -lpixman -lpng -lz -lmpfr -lgmp -lm -o gtk-test
 
 clean: 
 	rm -f mpsketch test.[0-9] test.log test.mpx mpxerr.tex
--- gtk-test.c
+++ gtk-test.c
@@ -1,7 +1,10 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <gdk/gdkkeysyms.h>
 #include <math.h>
+#include <ctype.h>
+#include <unistd.h>
 
 #include "paths.h"
 #include "mptoraster.h"
@@ -13,8 +16,39 @@ menus? open, help, quit, show/hide trace, change units, precision, change figure
 may be possible to display the vector graphics without converting to png first: http://stackoverflow.com/questions/3672847/how-to-embed-evince
 */
 
+#define SCROLL_STEP 10 //How many pixels to scroll at a time
+
+#define CURVE_MODE 0
+#define STRAIGHT_MODE 1
+#define CORNER_MODE 2
+#define CIRCLE_MODE 3
+
 cairo_surface_t *mp_png;  
+
 cairo_surface_t *trace;  
+char *trace_filename;
+int trace_x_offset,trace_y_offset;
+bool show_trace = false;
+
+char *job_name; //The part of the metapost filename before ".mp"
+unsigned int fig_num; //metapost figure number (the "1" in "beginfig(1)" for example)
+
+int mode=CURVE_MODE; //default drawing mode
+bool finished_drawing=true; //if true then we're ready to start drawing another path or circle.
+
+int density = 100; //points per inch for bitmap. changes when we zoom in
+int x_offset=0; //for scrolling
+int y_offset=0;
+unsigned int win_height = 600;
+unsigned int win_width = 800;
+
+// metapost coords of lower left corner of image
+float ll_x = 0;
+float ll_y = 0;
+
+//when we mouse over a point in a completed path, we can edit it
+bool edit=false;
+int edit_point; //which point are we editing
 
 void show_error(gpointer window) {//http://zetcode.com/gui/gtk2/gtkdialogs/
 	GtkWidget *dialog;
@@ -29,10 +63,12 @@ void show_error(gpointer window) {//http://zetcode.com/gui/gtk2/gtkdialogs/
 }
 
 static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
-	cairo_set_source_surface(cr, trace, 10, 10);
-	cairo_paint(cr);
+	if (show_trace) {
+		cairo_set_source_surface(cr, trace, trace_x_offset, trace_y_offset);
+		cairo_paint(cr);
+	}
 
-	cairo_set_source_surface(cr, mp_png, -100, 10);
+	cairo_set_source_surface(cr, mp_png, x_offset, y_offset);
 	cairo_paint(cr);
 
 	cairo_set_source_rgb(cr, 0, 0, 0);
@@ -67,7 +103,6 @@ static gboolean button_release(GtkWidget *widget, GdkEventButton *event, gpointe
 }
 
 static gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer user_data) {
-	printf("%d\n",event->keyval);
 	///usr/include/gtk-3.0/gdk/gdkkeysyms.h
 	switch(event->keyval) {
 		case GDK_KEY_e: ;
@@ -82,13 +117,15 @@ static gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer use
 			gtk_widget_destroy(widget);
 			break;
 		case GDK_KEY_y: ;
-			GtkClipboard *clip = gtk_clipboard_get(GDK_SELECTION_CLIPBOARD);
-			gtk_clipboard_set_text(clip, "test", -1);
+			gtk_clipboard_set_text(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD), "test", -1);
 			break;
 		case GDK_KEY_p: ;
 			gchar *text = gtk_clipboard_wait_for_text(gtk_clipboard_get(GDK_SELECTION_CLIPBOARD));
 			if (text)
 				printf("%s\n",text);
+			break;
+		default:
+			printf("%d\n",event->keyval);
 	}
 	return FALSE;
 }
@@ -101,12 +138,40 @@ static gboolean on_motion(GtkWidget *widget, GdkEventMotion *event, gpointer use
 	return FALSE;
 }
 
+static gboolean on_scroll(GtkWidget* widget, GdkEventScroll* event, gpointer user_data) {
+	switch (event->direction) {
+		case GDK_SCROLL_UP:
+			y_offset += SCROLL_STEP;
+			break;
+		case GDK_SCROLL_DOWN:
+			y_offset -= SCROLL_STEP;
+			break;
+		case GDK_SCROLL_LEFT:
+			x_offset += SCROLL_STEP;
+			break;
+		case GDK_SCROLL_RIGHT:
+			x_offset -= SCROLL_STEP;
+			break;
+		case GDK_SCROLL_SMOOTH:
+			puts("Smooth scrolling not implemented yet. Sorry.");
+			break;
+	}
+	gtk_widget_queue_draw(widget);
+	return FALSE;
+}
+
 static void activate (GtkApplication* app, gpointer user_data) {
 	GtkWidget *window;
 	GtkWidget *darea;
 
 	mp_png = cairo_image_surface_create_from_png("test.png");
-	trace = cairo_image_surface_create_from_png("trace.png");
+
+	if (show_trace) {
+		trace_x_offset = 0;
+		trace_y_offset = 0;
+		trace = cairo_image_surface_create_from_png(trace_filename);
+		if (cairo_surface_status(trace) != CAIRO_STATUS_SUCCESS) show_trace = false;
+	}
 
 	window = gtk_application_window_new (app);
 	gtk_window_set_title (GTK_WINDOW (window), "MPSketch");
@@ -114,10 +179,12 @@ static void activate (GtkApplication* app, gpointer user_data) {
 	gtk_widget_add_events(window, GDK_BUTTON_PRESS_MASK);
 	gtk_widget_add_events(window, GDK_POINTER_MOTION_MASK);
 	gtk_widget_add_events(window, GDK_BUTTON_RELEASE_MASK);
+	gtk_widget_add_events(window, GDK_SCROLL_MASK);
 	g_signal_connect(window, "button-press-event", G_CALLBACK(clicked), NULL);
 	g_signal_connect(window, "button-release-event", G_CALLBACK(button_release), NULL);
 	g_signal_connect(window, "key-press-event", G_CALLBACK (on_key_press), NULL);
 	g_signal_connect(window, "motion-notify-event", G_CALLBACK (on_motion), NULL);
+	g_signal_connect(window, "scroll-event", G_CALLBACK (on_scroll), NULL);
 
 	darea = gtk_drawing_area_new();
 	gtk_container_add(GTK_CONTAINER(window), darea);
@@ -128,12 +195,76 @@ static void activate (GtkApplication* app, gpointer user_data) {
 }
 
 int main (int argc, char **argv) {
+	unit = 1; //default is to use units of postscript points (1/72 inches). 
+	int c;
+	strcpy(unit_name,"");
+	while ((c = getopt (argc, argv, "u:t:")) != -1){
+		switch (c)
+		{
+			case 'u': //units
+				;
+				/*Examples of usage:
+				u=5cm //print coords as '(2u,3u)' for example (meaning (10cm,15cm))
+				u=cm
+				5cm //print coords as (2,3) to mean (10cm,15cm)
+				u=72 //72 postscript points = 1 inch, so print '(2u,3u)' to mean (2in,3in)
+				72 //print '(2,3)' to mean (2in,3in)
+				mm //equivalent to mm=1mm
+				*/
+
+				bool valid;
+				//check for '=' in optarg
+				char *num = strchr(optarg,'=');
+				if (num) {
+					//take everything before the '=' as the unit_name
+					*num = '\0';
+					unit = string_to_bp(num+1,&valid); //need to do this before setting unit_name, because str_to_bp uses unit_name and an optarg like 'u=5u' shouldn't be valid
+					if (strlen(optarg) > sizeof(unit_name)-1) {
+						puts("Unit name too long");
+						return 1;
+					}
+					strcat(unit_name,optarg);
+				} else {
+					unit = string_to_bp(optarg,&valid);
+					if (isalpha(optarg[0]))
+						strcat(unit_name,optarg);
+				}
+
+				if (!valid) {
+					puts("Invalid measure in argument of -u");
+					puts("Usage: mpsketch -u [<unitname>=]<number>[cm|mm|in|pt|bp]");
+					puts("e.g. mpsketch -u u=2cm");
+					return 1;
+				}
+				//printf("%f,%s\n",unit,unit_name);
+				break;
+			case 't':
+				show_trace = true;
+				trace_filename = optarg;
+				break;
+			case '?':
+				//TODO: print usage message
+				return 1;
+			default:
+				abort ();
+		}
+	}
+	coord_precision = ceil(log10(unit)); //choose a sensible number of decimal places for coordinates
+
+	if (argc > optind) job_name = argv[optind];
+	else job_name = "test";
+	if (argc > optind+1) {
+		fig_num = atoi(argv[optind+1]);
+	} else fig_num = 1;
+	//TODO: create job_name.mp if it doesn't exist
+
 	GtkApplication *app;
 	int status;
 
 	app = gtk_application_new ("com.github.jennigorham.mpsketch", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-	status = g_application_run (G_APPLICATION (app), argc, argv);
+	status = g_application_run (G_APPLICATION (app), 1, argv); //Couldn't find any info on how to stop it from complaining about unknown options and dying, so I'm telling it there are none (argc = 1)
+	//status = g_application_run (G_APPLICATION (app), argc, argv);
 	g_object_unref (app);
 
 	cairo_surface_destroy(mp_png);
